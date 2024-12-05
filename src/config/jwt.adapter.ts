32a7d75ad15b7e7fc483bb21d0126d471310dd13
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2024-12-05 18:57:31 -0400

    [Auth] JwT Seed configuration

--- src/config/jwt.adapter.ts
+++ src/config/jwt.adapter.ts
@@ -1,11 +1,13 @@
 import jwt from "jsonwebtoken";
+import { envs } from "./envs";
 
+const JWT_SEED = envs.JWT_SEED;
 export class JwtAdapter {
   // DI?
 
   static async generateToken(payload: any, duration: string = "2h") {
     return new Promise((resolve) => {
-      jwt.sign(payload, "SEED", { expiresIn: duration }, (err, token) => {
+      jwt.sign(payload, JWT_SEED, { expiresIn: duration }, (err, token) => {
         if (err) return resolve(null);
 
         return resolve(token);
@@ -14,6 +16,8 @@ export class JwtAdapter {
   }
 
   static validateToken(token: string) {
+    throw new Error("Not implemented");
+
     return;
   }
 }

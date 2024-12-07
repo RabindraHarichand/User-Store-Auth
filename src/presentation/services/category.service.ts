d256ce0e6b70b7fe5f9469c69872b3c7f447285d
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2024-12-07 13:19:21 -0400

    [Category] Implementation of Pagination in Get Categories

--- src/presentation/services/category.service.ts
+++ src/presentation/services/category.service.ts
@@ -1,5 +1,10 @@
 import { CategoryModel } from "../../data";
-import { CreateCategoryDto, CustomError, UserEntity } from "../../domain";
+import {
+  CreateCategoryDto,
+  CustomError,
+  PaginationDto,
+  UserEntity,
+} from "../../domain";
 
 export class CategoryService {
   constructor() {}
@@ -28,17 +33,39 @@ export class CategoryService {
     }
   }
 
-  async getCategories() {
-    try {
-      const categories = await CategoryModel.find();
+  async getCategories(paginationDto: PaginationDto) {
+    const { page, limit } = paginationDto;
 
-      return categories.map((category) => {
-        return {
-          id: category.id,
-          name: category.name,
-          available: category.available,
-        };
-      });
+    try {
+      // const total = await CategoryModel.countDocuments();
+      // const categories = await CategoryModel.find()
+      //   .skip((page - 1) * limit)
+      //   .limit(limit);
+
+      const [total, categories] = await Promise.all([
+        CategoryModel.countDocuments(),
+        CategoryModel.find()
+          .skip((page - 1) * limit)
+          .limit(limit),
+      ]);
+
+      return {
+        page: page,
+        limit: limit,
+        total: total,
+        next: `/api/categories?page=${page + 1}&limit=${limit}`,
+        prev:
+          page - 1 > 0
+            ? `/api/categories?page=${page - 1}&limit=${limit}`
+            : null,
+        categories: categories.map((category) => {
+          return {
+            id: category.id,
+            name: category.name,
+            available: category.available,
+          };
+        }),
+      };
     } catch (error) {
       throw CustomError.internalServer();
     }

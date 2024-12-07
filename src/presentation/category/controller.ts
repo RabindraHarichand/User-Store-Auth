2ad4f31bd7ec5857dcd633deae4bc3645c12eb75
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2024-12-07 12:25:29 -0400

    [Category] Add Create Category service implementation as well as Get Categories Controller & Service Implementation

--- src/presentation/category/controller.ts
+++ src/presentation/category/controller.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
-import { CreateCategoryDto, CustomError } from "../../domain";
+import { CreateCategoryDto, CustomError, UserEntity } from "../../domain";
+import { CategoryService } from "../services/category.service";
 
 export class CategoryController {
   //DI
-  constructor() {}
+  constructor(private readonly categoryService: CategoryService) {}
 
   private handleError = (error: unknown, res: Response) => {
     if (error instanceof CustomError)
@@ -17,10 +18,17 @@ export class CategoryController {
   createCategory = async (req: Request, res: Response) => {
     const [error, createCategoryDto] = CreateCategoryDto.create(req.body);
     if (error) return res.status(400).json({ error });
-    res.json(createCategoryDto);
+
+    this.categoryService
+      .createCategory(createCategoryDto!, req.body.user)
+      .then((category) => res.status(201).json(category))
+      .catch((error) => this.handleError(error, res));
   };
 
   getCategories = async (req: Request, res: Response) => {
-    res.json("Get Categories");
+    this.categoryService
+      .getCategories()
+      .then((categories) => res.json(categories))
+      .catch((error) => this.handleError(error, res));
   };
 }

d339e74fd40054dd914a0d7f63d0b25535bc1107
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2024-12-07 12:49:39 -0400

    [Pagination] Add Pagination DTO

--- src/presentation/category/controller.ts
+++ src/presentation/category/controller.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from "express";
-import { CreateCategoryDto, CustomError, UserEntity } from "../../domain";
+import {
+  CreateCategoryDto,
+  CustomError,
+  PaginationDto,
+  UserEntity,
+} from "../../domain";
 import { CategoryService } from "../services/category.service";
 
 export class CategoryController {
@@ -26,9 +31,15 @@ export class CategoryController {
   };
 
   getCategories = async (req: Request, res: Response) => {
-    this.categoryService
-      .getCategories()
-      .then((categories) => res.json(categories))
-      .catch((error) => this.handleError(error, res));
+    const { page = 1, limit = 10 } = req.query;
+    const [error, paginationDto] = PaginationDto.create(+page, +limit);
+    if (error) return res.status(400).json({ error });
+
+    res.json(paginationDto);
+
+    // this.categoryService
+    //   .getCategories()
+    //   .then((categories) => res.json(categories))
+    //   .catch((error) => this.handleError(error, res));
   };
 }

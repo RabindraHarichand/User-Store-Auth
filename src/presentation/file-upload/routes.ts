5664b93cf8b43664ae5d0c1a3dfe54204190f9d1
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2024-12-08 15:55:02 -0400

    [FileUpload] Add express-fileupload package, to handle files from client. Add FileUploadService with definition of two methods, uploadSingle & uploadMultiple. Implementation of uploadSingle (missing file naming)

--- src/presentation/file-upload/routes.ts
+++ src/presentation/file-upload/routes.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import { AuthMiddleware } from "../middlewares/auth.middleware";
 import { FileUploadController } from "./controller";
+import { FileUploadService } from "../services/file-upload.service";
 
 export class FileUploadRoutes {
   static get routes(): Router {
     const router = Router();
 
-    // const fileUploadService = new FileUploadService();
-    const controller = new FileUploadController();
+    const fileUploadService = new FileUploadService();
+    const controller = new FileUploadController(fileUploadService);
 
     // Definir las rutas
     //api/upload/single<user|category|product>/

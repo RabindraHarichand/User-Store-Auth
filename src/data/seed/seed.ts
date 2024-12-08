7fa64c5e7f015b5ae655a685b276eac1ddf6e496
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2024-12-08 11:24:59 -0400

    [Seed] Implementation of seed to create users, categories & products. Add radomBetween0andX funtion to generate random numbers that represent Ids, for categories & user, that will be assigned to the products that are being created

--- src/data/seed/seed.ts
+++ src/data/seed/seed.ts
@@ -1,5 +1,11 @@
 import { envs } from "../../config";
-import { MongoDatabase } from "../mongo/mongo-database";
+import {
+  CategoryModel,
+  MongoDatabase,
+  ProductModel,
+  UserModel,
+} from "../mongo";
+import { seedData } from "./data";
 
 (async () => {
   await MongoDatabase.connect({
@@ -11,10 +17,41 @@ import { MongoDatabase } from "../mongo/mongo-database";
   MongoDatabase.disconnect();
 })();
 
+const randomBetween0andX = (x: number) => {
+  return Math.floor(Math.random() * x);
+};
 async function main() {
-  //1 Crear users
-  //2 Crear categories
-  //3 Crear products
+  //0 Borrar todo
+  await Promise.all([
+    UserModel.deleteMany(),
+    CategoryModel.deleteMany(),
+    ProductModel.deleteMany(),
+  ]);
+
+  //1. Create users
+  const users = await UserModel.insertMany(seedData.users);
+
+  //2. Create categories
+  const categories = await CategoryModel.insertMany(
+    seedData.categories.map((category) => {
+      return {
+        ...category,
+        user: users[0]._id,
+      };
+    })
+  );
+
+  //3. Create products
+  const products = await ProductModel.insertMany(
+    seedData.products.map((product) => {
+      return {
+        ...product,
+        user: users[randomBetween0andX(seedData.users.length - 1)]._id,
+        category:
+          categories[randomBetween0andX(seedData.categories.length - 1)]._id,
+      };
+    })
+  );
 
   console.log("SEEDED");
 }

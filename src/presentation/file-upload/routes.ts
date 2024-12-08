eb0b800f3de1769523b7549a47a7ead5c4908b15
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2024-12-08 14:16:16 -0400

    [FileUpload] Add file upload routes. Also add controller with methods uploadFile & uploadMultipleFiles

---
+++ src/presentation/file-upload/routes.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { AuthMiddleware } from "../middlewares/auth.middleware";
+import { FileUploadController } from "./controller";
+
+export class FileUploadRoutes {
+  static get routes(): Router {
+    const router = Router();
+
+    // const fileUploadService = new FileUploadService();
+    const controller = new FileUploadController();
+
+    // Definir las rutas
+    //api/upload/single<user|category|product>/
+    //api/upload/multiple<user|category|product>/
+    router.post("/single/:type", controller.uploadFile);
+    router.post("/multiple/:type", controller.uploadMultipleFiles);
+
+    return router;
+  }
+}
